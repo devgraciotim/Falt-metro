200a2ff5e5b161075e91bd3be4ea19d72986f950
João Graciotim <graciotim92@gmail.com>
2024-12-22 22:19:09 -0300

    feat: updated data-source for .env variables and added new .env.example

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,19 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "faltometro",
-  synchronize: true,
-  logging: false,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432", 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: !isProduction,
+  logging: !isProduction,
   entities: ["src/entities/**/*.ts"],
   migrations: ["src/migrations/**/*.ts"],
   subscribers: [],
