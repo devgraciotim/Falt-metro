d08b8f227501f65d434b3d55b0820f3254ac6a86
João Graciotim <graciotim92@gmail.com>
2024-12-22 22:37:08 -0300

    feat: updated tsconfig and created User entity

---
+++ src/entities/User.ts
@@ -0,0 +1,46 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  password!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  instituicion!: string;
+
+  @Column({ type: 'bytea' })
+  photo!: Buffer;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  account_created!: Date;
+
+  @Column({ default: false })
+  email_verified!: boolean;
+
+  @Column({ type: 'text', nullable: true })
+  bio!: string;
+
+  @Column()
+  country!: string;
+
+  @Column()
+  state!: string;
+
+  @Column()
+  city!: string;
+
+  @Column({ type: 'date' })
+  date_of_birth!: Date;
+
+  @Column({ type: 'jsonb', nullable: true })
+  social_links!: Record<string, any>;
+}
